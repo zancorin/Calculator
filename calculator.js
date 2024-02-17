c9bee766754c5ecca7eab65f44aad7979eb0bbfa
zancorin <138537313+zancorin@users.noreply.github.com>
2024-02-17 02:53:13 -0500

    Add operate function w/ switch statement

--- calculator.js
+++ calculator.js
@@ -23,3 +23,34 @@ function divide(g,h)
         return g/h;
     }
 }
+
+function operate(num1, operator, num2)
+{
+
+switch(operator)
+{
+    case '+':
+        return add(num1,num2);
+        break;
+    
+    case '-':
+        return subtract(num1,num2);
+        break;
+        
+    case '*':
+        return multiply(num1,num2);
+        break;
+        
+    case '/':
+        return divide(num1,num2);
+        break;    
+}
+}
+
+
+
+console.log(add(5,2));
+console.log(subtract(5,2));
+console.log(multiply(5,2));
+console.log(divide(5,2));
+console.log(operate(5,'-',2));
